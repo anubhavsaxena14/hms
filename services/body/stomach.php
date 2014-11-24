ddf03f0d4dbb6924c3563dc4a95897cc0771911b
anubhavsaxena14 <anubhavsaxena14992@gmail.com>
2014-11-24 21:12:28 +0530

    Create stomach.php

---
+++ services/body/stomach.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1 align="center" class="style3">STOMACH</h1>
+<h3>Introduction</h3>
+<p>The <strong>stomach</strong> is a muscular, hollow, dilated part of the alimentary canal which functions as the primary organ of the digestive tract.
+It is involved in the second phase of digestion, following mastication (chewing).
+The stomach is located between the esophagus and the small intestine.
+It secretes protein-digesting enzymes and strong acids to aid in food digestion,
+and also churns food (sent to it via oesophageal peristalsis)
+through smooth muscular contortions before sending partially-digested food to the small intestines.</p>
+<p><img src="../../images/stomach1.png" width="255" height="255" /></p>
+<p>The stomach lies between the esophagus and the duodenum (the first part of the small intestine). It is   on the left upper part of the abdominal cavity. The top of the stomach lies   against the diaphragm. Lying behind the stomach is the pancreas. The greater omentum hangs   down from the <em>greater curvature</em>.</p>
+<p><img src="../../images/stomach.png" width="629" height="199" /></p>
+<h2>Diseases of the stomach</h2>
+<p>Historically, it was widely believed that the highly acidic environment of the stomach would keep the stomach immune from infection.
+However, a large number of studies have indicated that most cases of peptic ulcers, gastritis, and stomach cancer are caused by <em>Helicobacter pylori</em> infection.</p>
+</body>
+</html>
