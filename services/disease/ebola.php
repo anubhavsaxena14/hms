5b0038c7384782a5e82153baa25909db39722b34
anubhavsaxena14 <anubhavsaxena14992@gmail.com>
2014-11-24 21:15:09 +0530

    Create ebola.php

---
+++ services/disease/ebola.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1 align="center">EBOLA</h1>
+<br />
+Ebola virus disease (EVD; also Ebola hemorrhagic fever, or EHF), or simply Ebola, is a disease of humans and other primates caused by ebolaviruses. Signs and symptoms typically start between two days and three weeks after contracting the virus as a fever, sore throat, muscle pain, and headaches. Then, vomiting, diarrhea and rash usually follow, along with decreased function of the liver and kidneys. At this time some people begin to bleed both internally and externally.[1] The disease has a high risk of death, killing between 25 percent and 90 percent of those infected with the virus, averaging out at 50 percent.[1] This is often due to low blood pressure from fluid loss, and typically follows six to sixteen days after symptoms appear.[2]
+<br /><h3>Signs and Symptoms</h3>
+<img src="../../images/ebola.png" /><br />
+The length of time between exposure to the virus and the development of symptoms of the disease is usually 2 to 21 days.[1][12] Estimates based on mathematical models predict that around 5% of cases may take greater than 21 days to develop.[13]
+
+Symptoms usually begin with a sudden influenza-like stage characterized by feeling tired, fever, pain in the muscles and joints, headache, and sore throat.[1][14][15] The fever is usually higher than 38.3 °C (100.9 °F).[16] This is often followed by vomiting, diarrhea and abdominal pain.[15] Next, shortness of breath and chest pain may occur, along with swelling, headaches and confusion.[15] In about half of the cases, the skin may develop a maculopapular rash (a flat red area covered with small bumps).[16]
+<img src="../../images/EbolaCycle.png" /><br />
+
+<br /><h3>Cause</h3>
+EVD in humans is caused by four of five viruses of the genus Ebolavirus. The four are Bundibugyo virus (BDBV), Sudan virus (SUDV), Taï Forest virus (TAFV) and one simply called Ebola virus (EBOV, formerly Zaire Ebola virus).[23] EBOV, species Zaire ebolavirus, is the most dangerous of the known EVD-causing viruses, and is responsible for the largest number of outbreaks.[24] The fifth virus, Reston virus (RESTV), is not thought to cause disease in humans, but has caused disease in other primates.[25][26] All five viruses are closely related to marburgviruses.[23]
+
+<br /><h3>Infection Control<h3/>
+People who care for those infected with Ebola should wear protective clothing including masks, gloves, gowns and goggles.[78] The US Centers for Disease Control (CDC) recommend that the protective gear leaves no skin exposed.[79] These measures are also recommended for those who may handle objects contaminated by an infected person's body fluids.[80] In 2014, the CDC began recommending that medical personnel receive training on the proper suit-up and removal of personal protective equipment (PPE); in addition, a designated person, appropriately trained in biosafety, should be watching each step of these procedures to ensure they are done correctly.[79] In Sierra Leone, the typical training period for the use of such safety equipment lasts approximately 12 days.[81]
+<img src="../../images/EbolaVHFisolation.png" /><br />
+</body>
+</html>
