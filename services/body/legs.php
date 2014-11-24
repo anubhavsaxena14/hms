687c0328f6b9414a1ac45cabe1ccfee761ef4634
anubhavsaxena14 <anubhavsaxena14992@gmail.com>
2014-11-24 21:12:10 +0530

    Create legs.php

---
+++ services/body/legs.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+<div align="center">
+  <h1 class="style1">LEGS</h1>
+</div>
+<h3>Introduction</h3>
+<p>A <strong>leg</strong> is a limb on an organism's body that supports the rest of the plant above the ground   between the ankle and the arm and the groin. and is used for locomotion. The end   of the leg farthest from the animal's body is often either modified or attached   to another structure that is modified to disperse the animal's weight on the   ground (such as a foot).</p>
+<p>In bipedal vertebrate   animals, the two lower limbs are usually referred to as the &quot;legs&quot; and the two   upper limbs as &quot;arms&quot; or &quot;wings&quot; as the case may be.</p>
+<p>In the anatomy of vertebrates,   including human beings, <em>leg</em> is also used to refer to the   entire limb, but its   precise definition refers<a href="#cite_note-0"> </a>only to the segment between the knee and the ankle. In vertebrate and human anatomy   this segment is also called the <em>shank</em>,<a href="#cite_note-3"></a>and the front (anterior) of the segment is called the <em>shin</em> or <em>pretibia</em>.</p>
+<p><img src="../../images/leg1.png" width="300" height="400" /></p>
+<p>In human anatomical   terms, the leg is the part of the lower limb that lies between the knee and the ankle,<a href="#cite_note-5"></a> the thigh is between the hip and knee and the   term &quot;lower limb&quot; is used to describe the colloquial leg. This article generally   follows the common usage.</p>
+<h3>Skeleton</h3>
+<p>The major (long) bones of the human leg are the femur (thighbone), tibia (shinbone), and fibula (the smaller, rear calf bone). The patella (kneecap) is the bone in front of   the knee. Most of the leg skeleton has bony prominences and margins that can be palpated, notable exceptions being   the hip joint,   and the neck and shaft of femur. Many of   these anatomical landmarks are used to define the extent of the leg: most   notably the anterior superior iliac spine,   the greater   trochanter, the superior margin of the medial condyle of tibia, and the medial   malleolus.</p>
+<p><img src="../../images/leg2.png" width="300" height="300" /></p>
+</body>
+</html>
