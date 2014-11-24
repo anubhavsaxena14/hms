a51b7264a575eec9cee8cadf2b5679e3d73f6433
anubhavsaxena14 <anubhavsaxena14992@gmail.com>
2014-11-24 21:13:46 +0530

    Create tension.php

---
+++ services/disease/tension.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1 align="center" class="style1">HYPERTENSION</h1>
+<p align="center" class="style1"><img src="../../images/tension.jpeg" width="166" height="149" /></p>
+<p><strong>Hypertension</strong> is a chronic medical condition in which the blood pressure is elevated. It is also referred to as <strong>high blood pressure</strong> or shortened to <strong>HT</strong>, <strong>HTN</strong> or <strong>HPN</strong>. The word &quot;hypertension&quot;, by itself, normally refers to systemic, arterial hypertension.</p>
+<p>Hypertension can be classified as either <strong>essential</strong> (primary) or <strong>secondary</strong>.  Essential or primary hypertension means that no medical cause can be  found to explain the raised blood pressure and represents about 90-95%  of hypertension cases.Secondary hypertension indicates that the high blood pressure is a result of (<em>i.e.</em>, secondary to) another condition, such as kidney disease or tumours (adrenal adenoma or pheochromocytoma).</p>
+Persistent hypertension is one of the risk factors for strokes, heart attacks, heart failure and arterial aneurysm, and is a leading cause of chronic renal failure.Even moderate elevation of arterial blood pressure leads to shortened life expectancy. At severely high pressures, defined as mean arterial pressures 50% or more above average, a person can expect to live no more than a few years unless appropriately treated.Beginning at a systolic pressure (which is peak pressure in the arteries, which occurs near the end of the cardiac cycle when the ventricles are contracting) of 115 mmHg and diastolic pressure (which is minimum pressure in the arteries, which occurs near  the beginning of the cardiac cycle when the ventricles are filled with  blood) of 75 mmHg (commonly written as 115/75 mmHg), cardiovascular disease (CVD) risk doubles for each increment of 20/10 mmHg.
+</body>
+</html>
