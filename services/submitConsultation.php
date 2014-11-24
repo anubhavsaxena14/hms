9b213dcc95efe3f1939902a3d2c6715df840d730
anubhavsaxena14 <anubhavsaxena14992@gmail.com>
2014-11-24 21:08:41 +0530

    Create submitConsultation.php

---
+++ services/submitConsultation.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h3>CONSULTATION DETAILS</h3><hr />
+Under Construction... (need to create database for submitted consultation form, show a preview to the person submitting it)
+</body>
+</html>
