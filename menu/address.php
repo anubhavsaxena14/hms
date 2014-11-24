20c84f704cb4ccfbe896ebf2278e967ce67ba60d
anubhavsaxena14 <anubhavsaxena14992@gmail.com>
2014-11-24 21:01:59 +0530

    Create address.php

---
+++ menu/address.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h3>Address</h3><hr />
+<address>
+Silchar Medical College and Hosiptal,
+Silchar,
+Chachar,
+Assam - 788014
+</address>
+<iframe width='600' height='350' frameborder='0' scrolling='no'  marginheight='0' marginwidth='0'   src='https://maps.google.com/maps?&amp;q=Silchar Medical College Masimpur, Silchar, Assam, India &amp;output=embed'></iframe>
+</body>
+</html>
