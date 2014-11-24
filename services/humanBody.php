d5394c50bd299f165f2caec9c9a2abd10d05fd46
anubhavsaxena14 <anubhavsaxena14992@gmail.com>
2014-11-24 21:10:46 +0530

    Create humanBody.php

---
+++ services/humanBody.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1><center>The Human Body</center></h1>
+<hr />
+<br />
+<br />
+<pre>
+<center><h3>Please click on the approproate area's in the picture to get further information about the organs</h3></center>
+<center><img src="../images/humanbody.jpg" width="350" height="350" alt="human body" usemap="#humanbody" /></center>
+<map name="humanbody">
+	<acronym title="Head Portion"><area shape="rectangle" coords="140,60,200,100" alt="head portion" href="body/head.php"></acronym>
+	<acronym title="Chest Portion"><area shape="rectangle" coords="140,120,200,150" alt="chest portion" href="body/chest.php"></acronym>
+	<acronym title="Stomach Portion"><area shape="rectangle" coords="140,160,200,180" alt="stomach portion" href="body/stomach.php"></acronym>
+	<acronym title="Legs Portion"><area shape="rectangle" coords="140,200,200,300" alt="legs portion" href="body/legs.php"></acronym>
+</map>
+</body>
+</html>
