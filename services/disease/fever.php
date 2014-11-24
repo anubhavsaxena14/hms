1779eb49e43268485be416de6d22c00d77d797c4
anubhavsaxena14 <anubhavsaxena14992@gmail.com>
2014-11-24 21:13:30 +0530

    Create fever.php

---
+++ services/disease/fever.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<div align="center" class="style1">
+  <h1>FEVER</h1>
+  <p><img src="../../images/fever.jpeg" width="183" height="134" /></p>
+  <p><strong>Fever</strong> (also known as <strong>pyrexia</strong> or <strong>controlled hyperthermia</strong>  is a common medical sign characterized by an elevation of temperature above the normal range of 36.5–37.5 °C (98–100 °F) due to an increase in the body temperature regulatory set-point This increase in set-point triggers increased muscle tone and shivering.</p>
+  <p>As a person's temperature increases there is generally a feeling of cold despite an increasing body temperature. Once the new temperature is  reached there is a feeling of warmth. A fever is one of the body's immune responses which attempts to neutralize a bacterial or viral infection.  A fever can be caused by many different conditions ranging from benign  to potentially serious. With the exception of very high temperatures,  treatment to reduce fever is often not necessary; however, antipyretic medications can be effective at lowering the temperature, and this may improve the affected person's comfort.</p>
+</div>
+</body>
+</html>
