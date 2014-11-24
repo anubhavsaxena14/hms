3a7891cc62080e4c4205fdc881b3f9eae296c558
anubhavsaxena14 <anubhavsaxena14992@gmail.com>
2014-11-24 21:14:51 +0530

    Create swineflu.php

---
+++ services/disease/swineflu.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1 align="center" class="style1">SWINE FLU</h1>
+<p align="center" class="style1"><img src="../../images/flu.jpeg" width="177" height="159" /></p>
+<strong>Swine influenza</strong> (also called <strong>pig influenza</strong>, <strong>swine flu</strong>, <strong>hog flu</strong> and <strong>pig flu</strong>) is an infection by any one of several types of swine influenza virus. <strong>Swine influenza virus</strong> (<strong>SIV</strong>) or <strong>S-OIV</strong> (<strong>swine-origin influenza virus</strong>) is any strain of the influenza family of viruses that is endemic in pigs.As of 2009, the known SIV strains include influenza C and the subtypes of influenza A known as <strong>H1N1</strong>, H1N2, H3N1, H3N2, and H2N3.
+<p>Swine influenza virus is common throughout pig populations  worldwide. Transmission of the virus from pigs to humans is not common  and does not always lead to human influenza, often resulting only in the production of antibodies in the blood. If transmission does cause human influenza, it is called zoonotic swine flu. People with regular exposure to pigs are at increased risk  of swine flu infection. The meat of an infected animal poses no risk of  infection when properly cooked.</p>
+</body>
+</html>
