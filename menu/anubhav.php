123be845ccb1ebadd37af91d3a6d30c70b69af4b
anubhavsaxena14 <anubhavsaxena14992@gmail.com>
2014-11-24 21:02:42 +0530

    Create anubhav.php

---
+++ menu/anubhav.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h3>About the Project Developer</h3><hr />
+<pre><font size="5"><b>Anubhav Saxena</b> is a student at National Institute of Technology, Silchar <br />of 2012-2016 batch.
+Currently, he is in fifth semester of Computer Engineering branch. <br />
+He developed "Hospital Management System" as part of his project work for
+Computer Network course.
+
+
+<b>Contact Detail:</b>
+Anubhav Saxena
+Department of Computer Engineering
+Third Year Student
+Phone:- +91-8011440824
+<a href="mailto:anubhavsaxena14992@gmail.com">mail</a>
+</font>
+</pre>
+</body>
+</html>
