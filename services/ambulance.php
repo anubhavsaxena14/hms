37487ef4fb0714191bd22f8cc6645ba3cbe1503f
anubhavsaxena14 <anubhavsaxena14992@gmail.com>
2014-11-24 21:10:24 +0530

    Create ambulance.php

---
+++ services/ambulance.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h3>AMBULANCE</h3><hr />
+<big>
+<pre>
+Ambulance facility can be availed directly by calling at any of the following numbers:
+
++91-3842-234555 - To book directly through the hospital. They will get you the ambulance nearest to you.
++91-98765432 00 to 99  - These 100 numbers are available 24-hours each day.
+108 - Through this you can ask for a government ambulance and ask them to drop you to our hospital.
++91-98765431 00 to 99 - These 00 ambulances are specifically for students and faculty at various institutions in Silchar.
+
+We will try to make arrangements for you at the earliest.
+</pre>
+</big>
+<img src="../images/ambulance.png" height="420px" ></img>
+</body>
+</html>
