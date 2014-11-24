4e7e540990242c60772113a0d7ba69b1e7cd62f6
anubhavsaxena14 <anubhavsaxena14992@gmail.com>
2014-11-24 21:12:46 +0530

    Create head.php

---
+++ services/body/head.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1 align="center" class="style1">HEAD</h1>
+<hr/>
+<p>&nbsp;</p>
+<h3>Introduction</h3>
+<h4>In anatomy, the <strong>head</strong> of an animal is the rostral part (from anatomical position) that usually
+comprises the brain, eyes, ears, nose and mouth (all of which aid in various sensory functions, such as sight,
+hearing, smell, and taste). </h4>
+<div>
+  <h4>Brain Diseases</h4>
+</div>
+<div id="content">
+  <!-- begin object display -->
+  <table border="0" cellspacing="0" cellpadding="0" width="100%">
+    <tbody>
+      <tr>
+        <td><table border="0" width="99%">
+          <tbody>
+            <tr>
+              <td><!-- Display BODY of current page object -->
+                    <p>When the brain is healthy it functions quickly and automatically.
+					However,&nbsp;when problems occur, the results can be devastating.
+					Some of the major types of disorders include:</p>
+					<img src="../../images/head2.jpg">
+                <ul>
+                      <li>neurogenetic diseases, eg. Huntington&rsquo;s disease and muscular dystrophy;</li>
+                  <li>developmental disorders, eg. cerebral palsy;</li>
+                  <li>degenerative diseases of adult life, eg.&nbsp;Parkinson&rsquo;s disease and Alzheimer&rsquo;s disease;</li>
+                  <li>metabolic diseases, eg.&nbsp;Gaucher&rsquo;s disease;</li>
+                  <li>cerebrovascular diseases, eg.&nbsp;stroke and vascular dementia;</li>
+                  <li>trauma, eg.&nbsp;spinal cord and head injury;</li>
+                  <li>convulsive disorders, eg.&nbsp;epilepsy;</li>
+                  <li>infectious diseases, eg.&nbsp;AIDS dementia; and</li>
+                  <li>brain tumors</li>
+                </ul></td>
+            </tr>
+          </tbody>
+        </table></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+<img src="../../images/head1.bmp" />
+</body>
+</html>
