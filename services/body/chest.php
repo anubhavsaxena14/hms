1d0b2877115a03ef1f8a034f0a89233bb05d2bc0
anubhavsaxena14 <anubhavsaxena14992@gmail.com>
2014-11-24 21:11:54 +0530

    Create chest.php

---
+++ services/body/chest.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<body>
+<div align="center" class="style1">
+  <h1>CHEST</h1>
+</div>
+<p>&nbsp;</p>
+<h3>Introduction</h3>
+<p>The <strong>chest</strong> is a part of the anatomy of humans and various other animals sometimes   referred to as the thorax.</p>
+<p><img src="../../images/Chest_labeled.png" width="500" height="300" alt="chest" /></p>
+In hominids, the <strong>chest</strong> is the region of the body between the neck and the abdomen, along with its   internal organs and   other contents. It is mostly protected and supported by the ribcage, spine, and shoulder girdle. Contents of the chest include the   following:
+<ul>
+  <li>organs
+    <ul>
+        <li>heart </li>
+      <li>lungs </li>
+    </ul>
+  </li>
+</ul>
+<ul>
+  <li>muscles 
+</li>
+</ul>
+<div>
+<div>
+    <div><div></div>
+    </div>
+  </div>
+</div>
+<ul>
+  <li>
+    <ul>
+      <li>major and minor pectoral muscles </li>
+      <li>trapezius muscles and neck </li>
+    </ul>
+  </li>
+  <li>internal structures
+    <ul>
+        <li>diaphragm </li>
+      <li>esophagus </li>
+      <li>trachea </li>
+      <li>xiphoid process </li>
+    </ul>
+  </li>
+  <li>arteries and veins
+    <ul>
+        <li>aorta </li>
+      <li>superior vena cava </li>
+      <li>inferior vena cava </li>
+      <li>pulmonary   artery </li>
+    </ul>
+  </li>
+  <li>bones
+    <ul>
+        <li>the shoulder socket containing the upper part of the humerus </li>
+      <li>scapula </li>
+      <li>sternum </li>
+      <li>thoracic   portion of the spine </li>
+      <li>collarbone </li>
+      <li>ribcage </li>
+      <li>Floating ribs</li>
+    </ul>
+  </li>
+  <li>external structures
+    <ul>
+        <li>nipples </li>
+      <li>mammary glands </li>
+      <li>The <em>V of the chest</em> is the area exposed by open-necked shirts.</li>
+    </ul>
+  </li>
+  <li>thoracic abdomen (stomach, kidney/adrenal, pancreas, spleen, and lower oesophagus) </li>
+</ul>
+<h2><img src="../../images/humanbody1.jpg" width="319" height="400" alt="human body" /></h2>
+<h2><span>Chest Diseases</span></h2>
+<p>Different types of diseases or conditions that can be experienced in the chest include pleurisy, flail chest, atelectasis, and the most common   condition, chest pain. These conditions can be caused by birth defects, trauma, or   in some cases because of heredity. It is important that all conditions are   discussed with a physician and not diagnosed to ensure that proper treatment is   given. Any condition that lowers the ability to breath deeply or decreases a   persons ability to cough is considered a   chest disease or condition.</p>
+<h2>Chest Pain</h2>
+<p>Chest pain can be the result of multiple issues including respiratory problems, digestive issues, musculoskeletal complications. The pain can   trigger cardiac issues as well. Not all pain that is felt is associated with the   heart, but it should not be taken lightly either. Symptoms can be different   depending on the cause of the pain. While cardiac   issues cause feelings of sudden pressure in the chest or a crushing pain in the   back, neck and arms, pain that is felt due to non cardiac issues gives a burning   feeling along the digestive track or pain when deep breaths are attempted. It is   important to understand that pains will be felt differently for each person even if they are experiencing the same condition. Only a patient will truly know if   the symptoms are something mild or serious.</p>
+<p>Chest pain is most commonly associated with a heart attack. If this condition is present in the body, discomfort will be felt in the chest that is   similar to a heavy weight being placed on the body. Sweating, shortness of   breath, lightheadedness, and irregular   heartbeat can also be experienced. If heart attack occurs, the bulk of   damage is caused during the first six hours, so getting the proper treatment   quickly as possible is important. Some people, especially those who are elderly   or have diabetes, may not have typical chest pain but may have many of the other   symptoms of a heart attack. It is important that these patients and their care   givers have a good understanding of heart attack symptoms.</p>
+</body>
+</html>
