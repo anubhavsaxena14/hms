e6cf8db69d4cd133b81ccefeb8e3488c43aa9449
anubhavsaxena14 <anubhavsaxena14992@gmail.com>
2014-11-24 21:16:30 +0530

    Create diabetes.php

---
+++ services/disease/diabetes.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1 align="center"><span class="style1">DIABETES</span></h1>
+<table border="1" cellspacing="0" cellpadding="10" width="138%" height="623">
+  <tr valign="top">
+    <td width="17%" height="413" bgcolor="#FFFF00"><p><img src="../../images/diabetes.jpg" width="480" height="270" /></p></td>
+    <td width="83%"><p>&nbsp;</p>
+    <p align="justify" class="style3">Diabetes   mellitus is a metabolic disorder in which either the pancreas fail to produce   enough effective insulin hormone or the body cells become resistant to the insulin   hormone's effect.  </p>
+      <p align="justify" class="style3">The food you eat is broken   down into a sugar called glucose. Glucose levels increase after a meal, but quickly   return to normal as body cells remove excess glucose from the bloodstream. A hormone   called insulin in the bloodstream helps cells take up glucose and use it as energy.   But in some people, either the pancreas cells do not make insulin or cells of   the body cannot use the insulin properly. Without insulin, cells are unable to   take up glucose. Instead, high levels of glucose builds up in the bloodstream   causing Diabetics (where it can cause damage to eyes, nerves, and blood vessels).</p>
+      </p>
+      <p align="justify" class="style3">There   are a number of factors, both genetic and environmental, that may increase a person's   risk of developing diabetes. People who have family members with diabetes are   at a higher risk for developing the disease. Although diabetes can run in families,   inheriting a particular gene mutation does not guarantee that a person will get   the disease. They must also have certain environmental factors that trigger diabetes,   which include obesity, inactivity, increasing age, and a history of gestational   diabetes.</p>
+    <p align="center" class="style3"><img src="../../images/diabetes_symptoms.png" width="504" height="558" /></p></td>
+  </tr>
+</table>
+<h2 align="left" class="style2">&nbsp;</h2>
+<p align="left">&nbsp;</p>
+<p align="justify" class="style3">&nbsp;</p>
+<h4 align="justify">&nbsp;</h4>
+<h4 align="justify"><span class="style5">Types</span>
+<hr /></h4>
+<p align="justify" class="style3">Diabetes are of two types. First    one appears in childhood, while the Second type normally affects people after    the age of 30 and is slightly more common in women than men.</p>
+<p align="justify" class="style3"><strong><span class="style6">Type I diabetes</span>  </strong>This disorder usually  develops in childhood or adolescence and occurs when the pancreas  produces far too little insulin or produces none at all. </p>
+<p align="justify" class="style3"><em><strong> Causes </strong></em> of type I  diabetes :  It  is usually caused by an abnormal bodily reaction, in which the immune system  destroys insulin-secreting cells in the pancreas. The child of a person who  has type I diabetes is at greater risk of developing the same type of diabetes. </p>
+<p align="justify" class="style3"><span class="style6"><strong>Type II diabetes </strong></span> is the most common form of  diabetes normally affects people after  the age of 30 and is slightly more common in women than men.  This is a condition  in which pancreas continues to secrete insulin, but cells in the  body become  resistant to its effects. It develops slowly and is often unnoticed for many  years.  Here the body produces  insulin but not enough to cope with the levels of carbohydrate and sugar in the  diet.   </p>
+<p align="justify" class="style3"><em><strong>Causes</strong></em> of type II diabetes : Genetics,  obesity, inactivity, age, history of gestational diabetics are the important factors  causing the type II diabetes.</p>
+<p align="justify" class="style3">&nbsp;</p>
+<h4 class="style5">Symptoms
+<hr /></h4>
+<p align="justify" class="style3">The symptoms include </p>
+<div align="justify" class="style3">
+  <ul>
+    <li>
+      <p>Excessive urination</p>
+    </li>
+    <li>
+      <p>Profound thirst and a dry mouth</p>
+    </li>
+    <li>
+      <p>Increased hunger</p>
+    </li>
+    <li>
+      <p>Fatigue, loss of weight</p>
+    </li>
+    <li>
+      <p>Insufficient sleep because of the need to          urinate at night</p>
+    </li>
+    <li>
+      <p>Itchiness of the vulva,            soreness and inflammation of the genital area in women</p>
+    </li>
+    <li>
+      <p>Blurry vision </p>
+    </li>
+  </ul>
+</div>
+<p align="justify" class="style3">&nbsp;</p>
+<p align="justify" class="style3">Although some of the symptoms of both forms of  diabetes are similar, type I diabetes tends to develop more quickly and become  more severe. </p>
+<p align="justify">&nbsp;</p>
+<p align="justify">&nbsp;</p>
+</body>
+</html>
