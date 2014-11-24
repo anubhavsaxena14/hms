d183714d9edce4bad7bfd5e0c52c3d88036dd456
anubhavsaxena14 <anubhavsaxena14992@gmail.com>
2014-11-24 21:04:00 +0530

    Create what-HMS.php

---
+++ menu/what-HMS.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body><big>
+<b>What is Hospital Management System?</b><br /><br />
+Hospital Management System is a health informatics tool
+that focuses mainly on the administrational needs of hospitals as well providing support for patients.
+<br /><br />
+In many implementations, a HMS is a comprehensive, integrated information system designed to manage all the aspects of a hospital's operation,
+such as medical, administrative, financial, and legal issues and the corresponding processing of services.
+<br /><br />
+Hospital Management System architecture has three main levels,
+Central Government Level, Territory Level, and Patient Carrying Level.
+Through, Hospital Management System one can organise the Hospital resources and get easy access to it at any time.
+<br /><br />
+Also, it promises to make the work environment paperless and thus, is a green technological approach.
+</big></body>
+</html>
