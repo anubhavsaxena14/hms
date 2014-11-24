740710e213d2b3c72d1a137876cb0b166c7aa269
anubhavsaxena14 <anubhavsaxena14992@gmail.com>
2014-11-24 21:11:06 +0530

    Create reception.php

---
+++ services/reception.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h3>RECEPTION</h3><hr />
+<p>
+You can inquire regarding:-
+<ul>
+<li>Presence of Doctor.</li>
+<li>Collecting form and token to book an appointment with a doctor. <br />Be sure to bring reference number of any previous appointment for verification</li>
+<li>Admitting charges and process for a patient.</li>
+<li>Special services for students, elder people, people working in distinguished <br />and recognised organisations, government and army officials.</li>
+<li>Visiting hours and collecting Passes to see patients.</li>
+<li>Ambulance service.</li>
+</ul>
+
+We have about 150 beds for patients, but it's better you call us up to shift your dearest to the hospital for care.
+<br />
+We have a promising staff which do take care of patients round clock.
+<br /><br />
+Please be sure to first check with us regarding paperwork before applying any insurance policy.
+<br /><br />
+For any queries, contact us at +91-3842-234500 (10 Lines) 24-hours.
+</p>
+</body>
+</html>
