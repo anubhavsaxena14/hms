4fef56fb24ed42cff5135880ede9df3bba01d559
anubhavsaxena14 <anubhavsaxena14992@gmail.com>
2014-11-24 21:15:30 +0530

    Create asthma.php

---
+++ services/disease/asthma.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<body>
+<div align="center">
+  <h1><u><span class="style1">ASTHMA
+    </span></u>  </h1>
+</div>
+<table border="1" cellspacing="0" cellpadding="10" width="150%" height="483">
+  <tr valign="top">
+    <td width="17%" height="481" bgcolor="#FFFF00"><p><img src="../../images/thumb_asthma_2.jpg" width="226" height="176" /></p>    </td>
+    <td width="83%"><p class="style20">Asthma  is one of the most scary medical conditions which can affect both  adults and children. Symptoms of asthma vary widely from person to  person and are sometimes even different within the same person.<br />
+        <br />
+Asthma is a disease of the airways- bronchi and bronchioles. It is  characterized of constriction of the smooth muscles of the airways, by  inflammation and excessive secretion of mucus. This causes narrowing of  the air passages, increasing the effort of breathing and reducing the  amount of oxygen available. 	
+
+</p>
+      <p class="style19">Causes </p>
+      <p class="style20">There is a genetic component to asthma where children of asthmatics are more prone to develop asthma. It can be triggered by :</p>
+      <p class="style20"> - viral infection of respiratory tract</p>
+      <p class="style20"> - inhaled irritants like tobacco, smoke, pollutants, and fumes</p>
+      <p class="style20"> - vigorous exercise or stress
+        - allergies to pollen, mold, and pets</p>
+      <p class="style20"> - exposure to cold air
+        Asthma attacks can be very severe which can lead to decreased oxygen in the blood requiring external ventilation.</p>
+      <p class="style20"> Death caused by asthma is not uncommon.</p>
+      <p class="style19"> Symptoms</p>
+      <p class="style20"> Symptoms vary from person to person. They include dry cough, shortness of breath, chest tightness, and audible wheezing. In severe cases, there can be increased heart rate, sweating, extreme anxiety and inability to speak without gasping for air. 
+        
+        Emergency symptoms include bluish color of the face and lips, extremely labored respiration, mental confusion, and exhaustion. These symptoms require immediate medical attention. </p>
+      <p class="style19">Diagnosis        </p>
+      <p class="style19"><span class="style21">History</span>:</p>
+      <p class="style20"> Questions include:        </p>
+      <p class="style20">-what activities cause onset of symptoms,</p>
+      <p class="style20"> -if there are any exposure to irritants, cold air, exercise</p>
+      <p class="style20"> -any recent illnesses or exposure to any sick contacts</p>
+      <p class="style20"> -what, if any, home medications have been used thus far.</p>
+      <p> <span class="style23">Examination</span>:</p>
+      <p class="style20"> -Physical exam by medical personnel        </p>
+      <p><span class="style23">Diagnostic tests include</span>:</p>
+      <p class="style20"> -Chest x-ray</p>
+      <p class="style20"> -Pulmonary function tests        </p>
+      <p class="style20">-incentive spirometry</p>
+      <p class="style20"> -skin test for allergens.</p>
+      <p>&nbsp;</p>
+    <p>&nbsp;</p></td>
+  </tr>
+</table>
+<h1>&nbsp;</h1>
+<p>&nbsp;</p>
+</body>
+</html>
