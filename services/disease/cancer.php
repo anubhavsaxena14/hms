d9589f5fc6bd8a9be0eb055e66aaecbbdee34b55
anubhavsaxena14 <anubhavsaxena14992@gmail.com>
2014-11-24 21:16:45 +0530

    Create cancer.php

---
+++ services/disease/cancer.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1 align="center" class="style1">CANCER</h1>
+<p align="center" class="style1"><img src="../../images/cancer.jpeg" width="190" height="183" /></p>
+<p><strong>Cancer</strong> medical term: malignant neoplasm) is a class of diseases in which a group of cells display <em>uncontrolled growth</em> (division beyond the normal limits), <em>invasion</em> (intrusion on and destruction of adjacent tissues), and sometimes <em>metastasis</em> (spread to other locations in the body via lymph or blood). These three  malignant properties of cancers differentiate them from benign tumors, which are self-limited, and do not invade or metastasize. Most cancers form a tumor but some, like leukemia, do not. The branch of medicine concerned with the study, diagnosis, treatment, and prevention of cancer is oncology.</p>
+<p>Cancer affects people at all ages with the risk for most types increasing with age.Cancer caused about 13% of all human deaths in 2007(7.6 million).</p>
+<p>Cancers are caused by abnormalities in the genetic material of the transformed cells.These abnormalities may be due to the effects of carcinogens, such as tobacco smoke, radiation, chemicals, or infectious agents. Other cancer-promoting genetic abnormalities may randomly occur through errors in DNA replication, or are inherited, and thus present in all cells from birth. The heritability of cancers is usually affected by complex interactions between carcinogens and the host's genome.</p>
+
+</body>
+</html>
