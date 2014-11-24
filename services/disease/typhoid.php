eed40a5c74a0eb271d6896fe57c312ed4370a5c3
anubhavsaxena14 <anubhavsaxena14992@gmail.com>
2014-11-24 21:14:08 +0530

    Create typhoid.php

---
+++ services/disease/typhoid.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<div align="center" class="style1">
+  <h1>TYPHOID</h1>
+  <p><img src="../../images/typhoid.jpeg" width="190" height="147" /></p>
+  <p><strong>Typhoid fever</strong>, also known as <strong><em>Salmonella</em> Typhi</strong> or commonly just <strong>typhoid</strong>,is a common worldwide illness, transmitted by the ingestion of food or water contaminated with the feces of an infected person.The bacteria then perforate through the intestinal wall and are phagocytosed by macrophages. The organism is a Gram-negative short bacillus that is motile due to its peritrichous flagella. The bacterium grows best at 37 °C/99 °F – human body temperature.</p>
+  <p>This fever received various names, such as <strong>gastric fever</strong>, <strong>the bends</strong>, <strong>abdominal typhus</strong>, <strong>infantile remittent fever</strong>, <strong>slow fever</strong>, <strong>nervous fever</strong>, <strong>pythogenic fever</strong>, etc. The name of &quot; typhoid &quot; was given by Louis in 1829, as a derivative from typhus.</p>
+  <p>The impact of this disease falls sharply with the application of modern sanitation techniques.</p>
+</div>
+</body>
+</html>
