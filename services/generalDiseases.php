a3fceedc72ebbdfa30d9271916f5e7c24467acf8
anubhavsaxena14 <anubhavsaxena14992@gmail.com>
2014-11-24 21:10:01 +0530

    Create generalDiseases.php

---
+++ services/generalDiseases.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h3>GENERAL DISEASES</h3><hr />
+<ul>
+<li><a href="disease/asthma.php"/>Asthma</li>
+<li><a href="disease/cancer.php"/>Cancer</li>
+<li><a href="disease/diabetes.php"/>Diabetes</li>
+<li><a href="disease/ebola.php"/>Ebola</li>
+<li><a href="disease/fever.php"/>Fever</li>
+<li><a href="disease/swineflu.php"/>Swine Flu</li>
+<li><a href="disease/tension.php"/>Hypertension</li>
+<li><a href="disease/tuberculosis.php"/>Tuberculosis</li>
+<li><a href="disease/typhoid.php"/>Typhoid</li>
+</ul>
+</body>
+</html>
