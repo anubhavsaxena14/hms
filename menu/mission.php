57ad4ff5ac5fb4439f7844a76ca0487eb7f8ddf3
anubhavsaxena14 <anubhavsaxena14992@gmail.com>
2014-11-24 21:03:07 +0530

    Create mission.php

---
+++ menu/mission.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+<big>
+<b>How do we enhance your hospital experience?</b><br />
+We envision to streamline access to Hospital resources
+and provide hassle free services to customers and administration alike in following areas:
+<ul>
+<li>Provide Emergency Contact Details</li>
+<li>Provide Service for Ambulance contact</li>
+<li>Provide a deep understanding to some of the common diseases</li>
+<li>Provide understanding to diseases based upon study of Human Body </li>
+<li>Provide online consultancy service to users through submission of their symptoms</li>
+<li>Provide online appointment service to users</li>
+<li>Provide users with an account in which they can go through their previous records</li>
+<li>Provide latest additions to the hospital through News bulletin</li>
+</ul> <br />
+
+<u>You can navigate to the services section in order to get most out of it!</u>
+</big>
+</body>
+</html>
