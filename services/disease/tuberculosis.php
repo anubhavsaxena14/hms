7ae2ffd83841bcd98c69ef7b8b9d7219c12954a9
anubhavsaxena14 <anubhavsaxena14992@gmail.com>
2014-11-24 21:14:29 +0530

    Create tuberculosis.php

---
+++ services/disease/tuberculosis.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1 align="center" class="style1" id="firstHeading">Tuberculosis (T.B.)</h1>
+<table border="1" cellspacing="0" cellpadding="10" width="150%" height="483">
+  <tr valign="top">
+    <td width="14%" height="481" bgcolor="#FFFF00"><p><img src="../../images/Mycobacterium_tuberculosis.jpg" width="220" height="212" /></p>
+        <p align="left"><em><a  title="Mycobacterium tuberculosis"> &nbsp;</a></em><a  title="Mycobacterium tuberculosis" class="style5"> Mycobacterium tuberculosis</a></p>
+      <p>&nbsp;</p>
+      <p><img src="../../images/220px-TB_poster.jpg" width="220" height="342" />Public health campaigns tried to halt the spread of TB</p></td>
+    <td width="86%"><p><span class="style5"><strong>Tuberculosis</strong> or <strong>TB</strong> (short for <em>Tubercles <a title="Bacillus (shape)">Bacillus</a></em>) is a common and often deadly <a title="Infectious disease">infectious disease</a> caused by <a  title="Mycobacterium">mycobacteria</a>, usually <em><a title="Mycobacterium tuberculosis">Mycobacterium tuberculosis</a></em> in humans. Tuberculosis usually attacks the <a  title="Lung">lungs</a> but can also affect other parts of the body. It is spread through the  air, when people who have the disease cough, sneeze, or spit. Most infections in humans result in an <a  title="Asymptomatic">asymptomatic</a>,  latent infection, and about one in ten latent infections eventually  progresses to active disease, which, if left untreated, kills more than  50% of its victims.</span></p>
+    <p class="style5">The classic symptoms are a <a title="Cough">chronic cough</a> with <a title="Hemoptysis">blood-tinged</a> <a  title="Sputum">sputum</a>, <a  title="Fever">fever</a>, <a title="Night sweats">night sweats</a>, and <a  title="Weight loss">weight loss</a>. Infection of other organs causes a wide range of symptoms. <a title="Medical diagnosis">Diagnosis</a> relies on <a  title="Tuberculosis radiology">radiology</a> (commonly <a  title="Chest X-ray">chest X-rays</a>), a <a title="Mantoux test">tuberculin skin test</a>, blood tests, as well as microscopic examination and <a title="Microbiological culture">microbiological culture</a> of bodily fluids. <a  title="Tuberculosis treatment">Treatment</a> is difficult and requires long courses of multiple antibiotics. Contacts are also screened and treated if necessary. <a title="Antibiotic resistance">Antibiotic resistance</a> is a growing problem in (<a  title="Extensively drug-resistant tuberculosis">extensively</a>) <a title="Multi-drug-resistant tuberculosis">multi-drug-resistant tuberculosis</a>. Prevention relies on screening programs and <a title="Vaccination">vaccination</a>, usually with <em>Bacillus Calmette-Guérin</a> vaccine.</em></p>
+    <p><img src="../../images/676px-Tuberculosis_symptoms.png" width="676" height="599" /></p></td>
+  </tr>
+</table>
+<p align="center" class="style1">&nbsp;</p>
+<h3><span class="style6" id="Transmission">Transmission</span>
+<hr /></h3>
+<p class="style5">When people suffering from active pulmonary TB cough, sneeze, speak, or spit, they expel infectious <a "Particulate">aerosol</a> droplets 0.5 to 5 <a  title="?m">µm</a> in diameter. A single sneeze can release up to 40,000 droplets.Each one of these droplets may transmit the disease, since the  infectious dose of tuberculosis is very low and inhaling less than ten  bacteria may cause an infection.</p>
+<p class="style5">People with prolonged, frequent, or intense contact are at  particularly high risk of becoming infected, with an estimated 22%  infection rate. A person with active but untreated tuberculosis can  infect 10–15 other people per year.Others at risk include people in areas where TB is common, people who  inject drugs using unsanitary needles, residents and employees of  high-risk congregate settings, medically under-served and low-income  populations, high-risk racial or ethnic minority populations, children  exposed to adults in high-risk categories, patients <a  title="Immunocompromised">immunocompromised</a> by conditions such as <a  title="HIV">HIV</a>/<a  title="AIDS">AIDS</a>, people who take immunosuppressant drugs, and health care workers serving these high-risk clients.</p>
+<h2>&nbsp;</h2>
+<h2><span>Prevention</span>
+  <hr />
+</h2>
+<p class="style5">TB prevention and control takes two parallel approaches. In the  first, people with TB and their contacts are identified and then  treated. Identification of infections often involves testing high-risk  groups for TB. In the second approach, children are <a  title="Vaccination">vaccinated</a> to protect them from TB. No <a  title="Vaccine">vaccine</a> is available that provides reliable protection for adults. However, in  tropical areas where the levels of other species of mycobacteria are  high, exposure to <a itle="Nontuberculous mycobacteria">nontuberculous mycobacteria</a> gives some protection against TB.</p>
+<h3 class="style7"><span id="Vaccines">Vaccines</span> : </h3>
+<p class="style5">-<a title="Bacillus Calmette-Guérin">Bacillus Calmette-Guérin</a> (<em><strong>BCG</strong></em>)</p>
+<h2>&nbsp;</h2>
+<h2><span>Screening</span>
+  <hr />
+</h2>
+<div>
+  <div><a><img alt="" src="../../images/tuber_220px-Mantoux_tuberculin_skin_test.jpg" width="220" height="144" /></a>
+    <div>
+      <div></div>
+        <span class="style5"><a  title="Mantoux test">Mantoux tuberculin skin test</a></span></div>
+  </div>
+</div>
+<p class="style5"><a  title="Mantoux test">-Mantoux tuberculin skin tests</a> are often used for routine screening of high risk individuals.</p>
+<p class="style5"><a  title="Interferon-? release assays">-Interferon-? release assays</a> are blood tests used in the diagnosis of some infectious diseases.  </p>
+<p class="style5"><a  title="Chest photofluorography">-Chest photofluorography</a> has been used in the past for mass screening for tuberculosis.</p>
+<h2 class="style5">&nbsp;</h2>
+<h2>&nbsp;</h2>
+<h2><span>Treatment</span>
+  <hr />
+</h2>
+<p class="style5">Treatment for TB uses <a  title="Antibiotics">antibiotics</a> to kill the bacteria. Effective TB treatment is difficult, due to the  unusual structure and chemical composition of the mycobacterial cell  wall, which makes many antibiotics ineffective and hinders the entry of  drugs. The two antibiotics most commonly used are <a  title="Rifampicin">rifampicin</a> and <a  title="Isoniazid">isoniazid</a>.  However, instead of the short course of antibiotics typically used to  cure other bacterial infections, TB requires much longer periods of  treatment (around 6 to 24 months) to entirely eliminate mycobacteria  from the body. Latent TB treatment usually uses a single antibiotic, while active TB  disease is best treated with combinations of several antibiotics, to  reduce the risk of the bacteria developing <a  title="Antibiotic resistance">antibiotic resistance</a>.People with latent infections are treated to prevent them from progressing to active TB disease later in life.</p>
+<p>-<span class="style5">The</span><strong> <a  title="DOTS">DOTS</a> (Directly Observed Treatment Short-course)</strong> <span class="style5">strategy of tuberculosis  treatment recommended by WHO was based on clinical trials done in the  1970s by Tuberculosis Research Centre, Chennai, India.</span></p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</body>
+</html>
